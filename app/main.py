1344903224d324cbc3d85a4d222eeec554bcc33f
Dolinskiy Evgeniy <zkorvoz02@gmail.com>
2025-03-31 15:27:32 +0300

    fix

--- app/main.py
+++ app/main.py
@@ -5,22 +5,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-api_key = os.getenv("API_KEY")
-base_url = "https://api.weatherapi.com/v1/current.json"
-city = "Paris"
-country = "France"
-url = f"{base_url}?key={api_key}&q={city}"
+API_KEY = os.getenv("API_KEY")
+BASE_URL = "https://api.weatherapi.com/v1/current.json"
+CITY = "Paris"
+COUNTRY = "France"
+URL = f"{BASE_URL}?key={API_KEY}&q={CITY}"
 
 
 def get_weather() -> None:
     try:
-        response = requests.get(url)
+        response = requests.get(URL)
         if response.status_code == 200:
             weather_data = response.json()
             condition = weather_data["current"]["condition"]["text"]
             temp_c = weather_data["current"]["temp_c"]
             time = weather_data["location"]["localtime"]
-            print(f"{city}/{country} {time} Weather: {temp_c} Celsius, {condition}")
+            print(f"{CITY}/{COUNTRY} {time} Weather: {temp_c} Celsius, {condition}")
         else:
             print(f"Error: {response.status_code}, {response.text}")
     except Exception as e:
